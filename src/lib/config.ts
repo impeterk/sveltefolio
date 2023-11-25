d21362200237fb306faf4532479340b19a0e91dc
peter <peterkudelas@gmail.com>
2023-11-26 00:17:25 +0100

    added vercel adapter and edge runtime

---
+++ src/lib/config.ts
@@ -0,0 +1,6 @@
+import { dev } from '$app/environment';
+
+export const title = 'Peter Kudelas | Full stack web developer';
+export const description =
+	"Welcome to my portfolio! I'm a passionate full stack web developer from Slovakia";
+export const url = dev ? 'http://localhost:5173' : 'https://www.peterkudelas.eu';
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
 	import 'iconify-icon';
 	import Hero from '$lib/components/Hero.svelte';
+	import * as config from '$lib/config';
 </script>
 
+<svelte:head>
+	<title>{config.title}</title>
+	<meta name="description" content={config.description} />
+	<meta property="og:type" content="website" />
+	<meta property="og:url" content={config.url} />
+	<meta property="og:title" content={config.title} />
+	<meta property="og:description" content={config.description} />
+</svelte:head>
 <Hero />
