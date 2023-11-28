d7422f7a2226d88c120b776521ab643fff3dc663
Peter <storm.sk123@gmail.com>
2023-11-28 21:53:38 +0100

    removed prerender from sitemap

--- src/routes/sitemap[[page]].xml/+server.ts
+++ src/routes/sitemap[[page]].xml/+server.ts
@@ -2,8 +2,6 @@ import * as sitemap from 'super-sitemap';
 import type { RequestHandler } from '@sveltejs/kit';
 import { url } from '$lib/config';
 
-export const prerender = true;
-
 export const GET: RequestHandler = async ({ params }) => {
 	return await sitemap.response({
 		origin: url,
