ec926926f0bc37a4cb4fc162d2e6bdcd8d6f4f2f
Peter Kudelas <storm.sk123@gmail.com>
2023-12-26 13:22:49 +0100

    Update ProjectMobile.svelte

--- src/lib/components/projectPartials/ProjectMobile.svelte
+++ src/lib/components/projectPartials/ProjectMobile.svelte
@@ -14,7 +14,7 @@
 				<div class="artboard h-[600px] min-w-[280px] max-w-[300px] bg-dark">
 					{#key project.id}
 						<div
-							class="h-full w-full bg-base-100"
+							class="h-full w-full"
 							in:fly={{ x: '50%', duration: 500, delay: 500 }}
 							out:fly={{ x: '-100%', duration: 500 }}
 						>
