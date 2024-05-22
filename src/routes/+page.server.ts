02ead02560c4b22b1e718372ded258b28fc65c36
Peter Kudelas <storm.sk123@gmail.com>
2024-05-22 17:26:49 +0000

    disable nodemailer

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,5 @@
 import type { Actions } from './$types';
-import { mailer } from '$lib/server/mailer';
+// import { mailer } from '$lib/server/mailer';
 
 export const actions: Actions = {
 	setTheme: async ({ url, cookies }) => {
@@ -13,22 +13,22 @@ export const actions: Actions = {
 		}
 	},
 	contact: async({request}) => {
-		const data = await request.formData()
-		const email = data.get('email') as string;
-		const name = data.get('name') as string
-		const message = data.get('message') as string
+		// const data = await request.formData()
+		// const email = data.get('email') as string;
+		// const name = data.get('name') as string
+		// const message = data.get('message') as string
 
 
-		const response = await mailer({email, name, message})
+		// // const response = await mailer({email, name, message})
 		
-		if (response) {
-			return {
-				success: true
-			}
-		} else {
-			return {
-				error: true
-			}
-		}
+		// if (response) {
+		// 	return {
+		// 		success: true
+		// 	}
+		// } else {
+		// 	return {
+		// 		error: true
+		// 	}
+		// }
 	}
 };
