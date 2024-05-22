ec8c3d7c9a21ecde98ae34ab213ce14c3af9d8f0
Peter Kudelas <storm.sk123@gmail.com>
2024-05-22 18:26:06 +0000

    update route

--- src/routes/mail/+server.ts
+++ src/routes/mail/+server.ts
@@ -3,8 +3,10 @@ import type { RequestHandler } from './$types';
 import { mailer } from '$lib/server/mailer';
 
 export const POST: RequestHandler = async ({request}) => {
-    const {email, name, message} = request.json()
-
+    console.log({request})
+    const {email, name, message} = await request.json()
+    
+    console.log({email, name, message})
     const response = await mailer({email, name, message})
 
     if (!response) {
