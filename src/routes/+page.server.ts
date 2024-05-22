cf5a05255ad8b237bb61e92f23957294a64463b8
Peter Kudelas <storm.sk123@gmail.com>
2024-05-22 18:22:02 +0000

    update contact action

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,4 @@
 import type { Actions } from './$types';
-// import { mailer } from '$lib/server/mailer';
 
 export const actions: Actions = {
 	setTheme: async ({ url, cookies }) => {
@@ -13,22 +12,25 @@ export const actions: Actions = {
 		}
 	},
 	contact: async({request}) => {
-		// const data = await request.formData()
-		// const email = data.get('email') as string;
-		// const name = data.get('name') as string
-		// const message = data.get('message') as string
+		const data = await request.formData()
+		const email = data.get('email') as string;
+		const name = data.get('name') as string
+		const message = data.get('message') as string
 
 
-		// const response = await mailer({email, name, message})
+		const response = await fetch("https://sveltefolio.vercel.app/api/mail", {
+			method: 'POST', 
+		body: data
+	})
 		
-		// if (response) {
-		// 	return {
-		// 		success: true
-		// 	}
-		// } else {
-		// 	return {
-		// 		error: true
-		// 	}
-		// }
+		if (response.ok) {
+			return {
+				success: true
+			}
+		} else {
+			return {
+				error: true
+			}
+		}
 	}
 };
